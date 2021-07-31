3ba552e5b414e39486a2dfde64a32bcb8baf7054
BenJarrett <b.cl.jarrett@gmail.com>
2021-07-31 12:36:33 -0500

    Created a car class. Defined it's properties and created 3 methods. Made an Enum for CarColor as well

---
+++ YourGarage/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourGarage
+{
+    class Car
+    {
+        public int BatteryCapacity { get; set; } = 50;
+        public CarColor Color { get; set; }
+        public string PassengerOccupancy { get; set; }
+
+
+        public void Refuel()
+        {
+            if (BatteryCapacity <= 50)
+            {
+                BatteryCapacity = +50;
+            }
+            Console.WriteLine($"Your car is not at {BatteryCapacity} %.");
+        }
+
+        public void Driving()
+        {
+            Console.WriteLine($"The {Color} car is driving on the freeway.");
+        }
+
+        public void Braking()
+        {
+            Console.WriteLine($"The {Color} car braked at the red light.");
+        }
+        public enum CarColor
+        {
+            Black,
+            Red,
+            White,
+            Blue
+        }
+
+    }
+}
